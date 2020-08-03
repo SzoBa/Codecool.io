799f2b11f0e3fc992bbb10d21c286b46b6c1cc6f
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-08-03 14:43:00 +0200

    Add room user data send

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect, session, jsonify, json, flash
 from flask_socketio import SocketIO, join_room, leave_room, emit
 
 
@@ -8,18 +8,14 @@ socketio = SocketIO(app)
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/room')
+def room():
+    return render_template('room.html')
 
 
 @app.route('/game')
 def game():
     return render_template('game.html')
-
-
-@app.route('/room')
-def room():
-    return render_template('room.html')
 
 
 if __name__ == '__main__':
--- static/js/room.js
+++ static/js/room.js
@@ -1,6 +1,14 @@
 init();
 
 function init() {
+    addSocket();
+    addListenerToButton();
+}
+
+
+// username: key - name
+
+function addSocket() {
     let socket = io('http://127.0.0.1:5000')
     socket.addEventListener('connect', (event) => {
         console.log('we are connected to server')
@@ -8,7 +16,16 @@ function init() {
     socket.addEventListener('close', (event) => {
         console.log('connection closed')
     })
+
 }
 
+function addListenerToButton() {
+    let button = document.querySelector('#username_button')
+    button.addEventListener('click', () => {
+        let username = document.querySelector('#username').value;
+        localStorage['username'] = username;
+        let userdata = {'username': username};
+        socket.emit('create-room', userdata);
+    })
 
-// username: key - name
+}
