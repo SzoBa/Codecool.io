39f0a5e90593df064a612e15d750894adc2bfea2
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-08-03 22:25:15 +0200

    Add room join listeners

--- static/js/room.js
+++ static/js/room.js
@@ -2,17 +2,20 @@ init();
 
 function init() {
     let socket = io('http://127.0.0.1:5000')
-    socket.addEventListener('connect', (event) => {
-        console.log('we are connected to server')
-    });
-    socket.addEventListener('close', (event) => {
-        console.log('connection closed')
-    })
-
+    addSocketConnectionListeners(socket);
     addListenerToButton(socket);
     addSocketListenerCreatedRoom(socket);
 }
 
+function addSocketConnectionListeners(socket) {
+    socket.addEventListener('connect', () => {
+        console.log('we are connected to server')
+    });
+    socket.addEventListener('close', () => {
+        console.log('connection closed')
+    })
+
+}
 
 function addListenerToButton(socket) {
     let button = document.querySelector('#username_button')
@@ -20,6 +23,7 @@ function addListenerToButton(socket) {
         let username = document.querySelector('#username').value;
         if (username) {
             localStorage['username'] = username;
+            //get room data if not exists - create, else join (or both - later)
             let userdata = {'username': username};
             socket.emit('create-room', userdata);
         }
@@ -29,22 +33,35 @@ function addListenerToButton(socket) {
 
 function addSocketListenerCreatedRoom(socket) {
     socket.addEventListener('own-room-created', (event) => {
-        console.log(event);
         localStorage['player_id'] = event.player_id;
-        let roomDiv = document.querySelector('#room_div');
-        roomDiv.innerHTML = ""
-        let createTable = `
-        <button>Create Room</button>`
-        roomDiv.insertAdjacentHTML('beforeend', createTable);
-
+        document.querySelector('#room_div').classList.add('display-none');
+        // let roomInnerDiv = document.querySelector('#room_div_inner');
+        // roomInnerDiv.innerHTML = ""
+        // let createTable = `
+        // <button id="create_room_button">Create Room</button>`
+        // roomInnerDiv.insertAdjacentHTML('beforeend', createTable);
+        // document.querySelector('#create_room_button').addEventListener('click', createRoom);
+        //she/he is already in the room, no need for create button here
     })
     socket.addEventListener('new-room-created', (event) => {
+        //it receives the creator's data
+        let creatorData = event['username'];
+        console.log(event)
         localStorage['player_id'] = event.player_id;
-        let roomDiv = document.querySelector('#room_div');
-        roomDiv.innerHTML = ""
+        let roomInnerDiv = document.querySelector('#room_div_inner');
+        roomInnerDiv.innerHTML = ""
         let createTable = `
-        <button>Join Room</button>`
-        roomDiv.insertAdjacentHTML('beforeend', createTable);
-
+        <button id="join_room_button" data-creator=${creatorData}>Join Room</button>`
+        roomInnerDiv.insertAdjacentHTML('beforeend', createTable);
+        document.querySelector('#join_room_button').addEventListener('click', joinRoom);
     })
+}
+
+function createRoom() {
+
+}
+
+function joinRoom(event) {
+    console.log(event.target.dataset.creator);
+
 }
