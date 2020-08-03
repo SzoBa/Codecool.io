0c18b74f9606a0f2499534b05a10168d078474f9
Márton Kozma <kozmartinus@gmail.com>
2020-08-03 14:38:32 +0200

    add sample_data.sql

---
+++ data/sample_data.sql
@@ -0,0 +1,32 @@
+ALTER TABLE IF EXISTS ONLY public.user DROP CONSTRAINT IF EXISTS pk_user_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.room DROP CONSTRAINT IF EXISTS pk_room_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.user DROP CONSTRAINT IF EXISTS fk_room_id CASCADE;
+
+DROP TABLE IF EXISTS public.user;
+CREATE TABLE user (
+    id serial NOT NULL,
+    name text,
+    password text,
+    room_id integer,
+    points integer DEFAULT 0,
+    is_drawer boolean NOT NULL DEFAULT false
+);
+
+DROP TABLE IF EXISTS public.room;
+CREATE TABLE room (
+    id serial NOT NULL,
+    word text,
+    max_round integer DEFAULT 3,
+    round_counter integer DEFAULT 0,
+    drawing_time integer DEFAULT 60,
+    is_open boolean NOT NULL DEFAULT true
+);
+
+ALTER TABLE ONLY user
+    ADD CONSTRAINT pk_user_id PRIMARY KEY (id);
+
+ALTER TABLE ONLY room
+    ADD CONSTRAINT pk_room_id PRIMARY KEY (id);
+
+ALTER TABLE ONLY user
+    ADD CONSTRAINT fk_room_id FOREIGN KEY (room_id) REFERENCES room(id) ON DELETE CASCADE;
