36b0ef2e5f041a12ad601f6982e41bfbee09c49f
Oliver Simon <oliversimon104@gmail.com>
2020-08-04 11:07:45 +0200

    Create guess-chat function

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,5 +1,22 @@
-function init() {
+function chatInputField() {
+    let inputField = document.querySelector('#chat-input');
+    let messageContainer = document.querySelector('.message-container');
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+    inputField.addEventListener('keyup', function (event) {
+        if (event.keyCode === 13) {
+            event.preventDefault()
+            let chatMessage = inputField.value
+            let chatMessageDiv = `<div class="message">${chatMessage}</div>`
+            messageContainer.insertAdjacentHTML('beforeend', chatMessageDiv)
+            inputField.value = ''
+            messageContainer.scrollTop = messageContainer.scrollHeight;
+        }
 
-}
+    })
+};
+
+function init() {
+    chatInputField()
+};
 
 init()
