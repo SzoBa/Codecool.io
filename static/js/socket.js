04a0f1857f81256866b0468197c7e65d2b7bfc91
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-08-04 13:50:39 +0200

    Add socket js

---
+++ static/js/socket.js
@@ -0,0 +1,12 @@
+export let socket = io('http://127.0.0.1:5000');
+
+init();
+
+function init() {
+    let roomId = localStorage.getItem('room_id');
+    socket.emit('join-game-start', roomId);
+
+    socket.addEventListener('alert', ()=> {
+        alert('hello');
+    })
+}
