7962126b6060d9003346638ae5b2851464bd9c44
sandyrnelson <sandyrnelson@berkeley.edu>
2020-08-04 10:38:07 +0200

    Add shaking to time

--- static/js/timer.js
+++ static/js/timer.js
@@ -9,9 +9,11 @@ function initTimer() {
         //the round ends
         clearInterval(timeCounter);
         timerElement.classList.remove("blinking");
+        timerElement.classList.remove("shake");
         clock.classList.remove("shake")
     }else if (currentTime <= 10){
         timerElement.classList.add("blinking");
+        timerElement.classList.add("shake");
         clock.classList.add("shake");
     }
     timerElement.textContent = (currentTime - 1).toString()}, 1000);
