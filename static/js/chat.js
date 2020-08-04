92dd4f4bea356554b0a9ff565b5e392f14b36003
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-08-04 17:49:52 +0200

    Add room chat function

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,3 +1,12 @@
+import {socket} from './socket.js'
+
+init();
+
+function init() {
+    chatInputField();
+    chatSocketSetup();
+}
+
 function chatInputField() {
     let inputField = document.querySelector('#chat-input');
     let messageContainer = document.querySelector('.message-container');
@@ -6,17 +15,21 @@ function chatInputField() {
         if (event.keyCode === 13) {
             event.preventDefault()
             let chatMessage = inputField.value
-            let chatMessageDiv = `<div class="message">${chatMessage}</div>`
-            messageContainer.insertAdjacentHTML('beforeend', chatMessageDiv)
+            let data = {message: chatMessage, username: localStorage.getItem('username'), room_id: localStorage.getItem('room_id')}
+            socket.emit('send-chat-message', JSON.stringify(data));
             inputField.value = ''
-            messageContainer.scrollTop = messageContainer.scrollHeight;
         }
 
     })
-};
+}
 
-function init() {
-    chatInputField()
-};
+function chatSocketSetup() {
+    socket.addEventListener('new-chat-message', (data)=> {
+        let messageData = JSON.parse(data);
+        let messageContainer = document.querySelector('.message-container');
+        let chatMessageDiv = `<div class="message">${messageData['username']}: ${messageData['message']}</div>`
+        messageContainer.insertAdjacentHTML('beforeend', chatMessageDiv);
+        messageContainer.scrollTop = messageContainer.scrollHeight;
+    })
+}
 
-init()
