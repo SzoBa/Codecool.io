2574eb78cb9fbb4772f0cce37e7bee710c1384ae
Balázs Szolcsánszki <bszolcsan@gmail.com>
2020-08-05 10:09:40 +0200

    Add get random word

--- static/js/game.js
+++ static/js/game.js
@@ -3,6 +3,23 @@ import {socket} from './socket.js'
 init();
 
 function init() {
+    if (localStorage.getItem('owner_id') === localStorage.getItem('player_id')) {
+        getWord();
+    }
+}
 
+function getWord() {
+    let wordNumber = Math.floor(Math.random() * 6801) + 1;
+    let roomId = localStorage.getItem('room_id');
+    let data = {word_number: wordNumber, room_id: roomId}
+    let url = '/get-word';
+    fetch(`${url}?data=${data}`)
+        .then((response) => response.json())
+        .then(data => {
+            useTheWord(data);
+        })
+}
 
+function useTheWord(word) {
+    console.log(word)
 }
