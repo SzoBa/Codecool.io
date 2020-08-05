f3cd294027bbe716a2c26766aeb7d003d73bc822
szabomidu <60429813+szabomidu@users.noreply.github.com>
2020-08-05 14:47:14 +0200

    Added a new item called can_guess to local storage

--- static/js/room.js
+++ static/js/room.js
@@ -45,6 +45,7 @@ function addSocketListenerCreatedRoom() {
         localStorage['user_id'] = event.player_id;
         localStorage['owner_id'] = event.player_id;
         localStorage['room_id'] = event.room_id;
+        localStorage['can_guess'] = true;
         document.querySelector('#room_div').classList.add('display-none');
         let currentRoom = document.querySelector('#current_room');
         let createdRoom = `
@@ -99,6 +100,7 @@ function joinRoom(event) {
     let ownerId = event.target.closest('div').querySelector('#join_room_button').dataset.creator;
     if (username) {
         localStorage['username'] = username;
+        localStorage['can_guess'] = true;
         let userdata = {'username': username, 'room_id': roomId, 'owner_id': ownerId};
         socket.emit('join-room', userdata);
         this.closest('div').querySelector('input').remove();
